9fb7c9bfb9f9c5fd121f830a8661417c38663bd0
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 19:14:23 +0530

    DATA BINDING COMPLETED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-secondary">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">PRODUCT APP</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import { useSearchParams } from 'react-router-dom'
 
 const ViewAll = () => {
+        const [data,changedata]=useState(
+            [
+                {"PRODUCT":"BOOK","PRICE":250},
+                {"PRODUCT":"PEN","PRICE":50},
+                {"PRODUCT":"EPNCIL","PRICE":100}
+            ]
+        )
     return (
         <div>
             <Navbar/>
@@ -9,106 +17,24 @@ const ViewAll = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BU</a>
-                                        </div>
+                          {data.map(
+                            (value,i) =>
+                                {
+                                    return   <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                    <div className="card" >
+                                        <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
+                                            <div className="card-body">
+                                                <h5 className="card-title">{value.PRODUCT}</h5>
+                                                <p className="card-text">{value.PRICE}</p>
+                                                <a href="#" class="btn btn-primary">BUY</a>
+                                            </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card" >
-                                    <img src="https://i2.wp.com/letterprint.co.in/wp-content/uploads/2018/07/stationary-products-1.jpg?fit=1000%2C700" className="card-img-top" alt="..." height="250px"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">BOOKS</h5>
-                                            <p className="card-text">240</p>
-                                            <a href="#" class="btn btn-primary">BUY</a>
-                                        </div>
-                                </div>
-                            </div>
+                                }
+                          )
+
+                          }
+                            
                             
                          
                         </div>
