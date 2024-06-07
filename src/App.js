9182c01cbe414bb3ba50ecabb104a58c62aa5cbe
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 18:34:49 +0530

    SEARCH COM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
+import SearchProduct from './components/SearchProduct';
 
 function App() {
   return (
     <div >
       <AddProduct/>
+      <SearchProduct/>
+      
     </div>
   );
 }
