99a9bc02b6a6d5f98f38bd8b900f073fa7cf9e8e
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 18:39:21 +0530

    DELETE COMPLETE

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
 import SearchProduct from './components/SearchProduct';
+import DeleteProduct from './components/DeleteProduct';
 
 function App() {
   return (
     <div >
       <AddProduct/>
       <SearchProduct/>
-      
+      <DeleteProduct/>
     </div>
   );
 }
